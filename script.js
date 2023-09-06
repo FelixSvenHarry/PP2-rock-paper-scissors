39c5ab7ac8e82605136c845f28946b6dfd86967f
FelixSvenHarry <>
2023-09-06 17:20:44 +0000

    add function to calculate outcome

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ function computerMath() {
     const choicesArrayIndex = Math.floor(Math.random() * 3);
     return choicesArray[choicesArrayIndex];
 }
+
+
+/**
+ * function to calculate the result by comparing the players move to the computers
+ */
+function calculateResult(playerChoice, computerChoice) {
+    if (playerChoice) {
+        computerChoiceElem.innerHTML = `Computer Choose: ${computerChoice}`;
+        if (playerChoice === computerChoice) {
+            result.innerHTML = `It's a draw, Choose a new "weapon"!!!`;
+        } else if (
+            (playerChoice === "rock" && computerChoice === "scissors") ||
+            (playerChoice === "paper" && computerChoice === "rock") ||
+            (playerChoice === "scissors" && computerChoice === "paper")
+        ) {
+            result.innerHTML = "You Win, Congratulations!!";
+            playerScore++;
+            playerScoreElem.innerHTML = `Player: ${playerScore}`;
+        } else {
+            result.innerHTML = "Computer Wins, better luck next time!!";
+            computerScore++;
+            computerScoreElem.innerHTML = `Computer: ${computerScore}`;
+        }
+    }
+}
+
